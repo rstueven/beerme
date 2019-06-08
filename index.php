4e0fa1af1032d76fe580da38e691b8bb72cfaa05
Richard Stueven <rstueven@beerme.com>
2019-06-08 12:11:41 -0500

    Test index, layout include, server

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php $documentTitle = "The most complete source of brewery information worldwide." ?>
+<?php include 'layouts/top.php'; ?>
+
+<h1>BEER ME!</h1>
+
+<?php phpinfo(); ?>
+
+<?php include 'layouts/bottom.php'; ?>
---
+++ layouts/top.php
@@ -0,0 +1,15 @@
+<?php echo "<?xml version='1.0' encoding='utf-8'?>\n"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <title>Beer Me! &mdash; <?php echo $documentTitle; ?></title>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta http-equiv="Content-Style-Type" content="text/css" />
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <!-- link type='text/css' rel='stylesheet' href='/css/beerme.css' /-->
+</head>
+
+<body>
