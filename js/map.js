8d43eaf3b0c479e8106be865f53c74cba85f202b
Richard Stueven <rstueven@beerme.com>
2020-08-09 15:33:10 -0500

    Working on My Location

--- js/map.js
+++ js/map.js
@@ -9,11 +9,12 @@ Map.init = function (lat, lng, zoom) {
       })
     ],
     controls: ol.control.defaults({
-      // attributionOptions: {
-      //   collapsed: true
-      // }
+      attributionOptions: {
+        collapsed: true
+      }
     }).extend([
-      new ol.control.ScaleLine()
+      new ol.control.ScaleLine(),
+      new ol.control.ZoomSlider()
     ]),
     view: new ol.View({
       center: ol.proj.fromLonLat([lng, lat]),
@@ -21,7 +22,30 @@ Map.init = function (lat, lng, zoom) {
     })
   });
 
-  // https://github.com/jonataswalker/ol-geocoder
+  // https://openlayers.org/workshop/en/mobile/geolocation.html
+  const source = new ol.source.Vector();
+  const layer = new ol.layer.Vector({
+    source: source
+  });
+  map.addLayer(layer);
+
+  navigator.geolocation.watchPosition(function (pos) {
+    console.log(pos.coords.longitude + " : " + pos.coords.latitude + " : " + pos.coords.accuracy);
+    // map.getView().setCenter(ol.proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]));
+    const coords = [pos.coords.longitude, pos.coords.latitude];
+    const accuracy = ol.geom.Polygon.circular(new ol.Sphere(6378137), coords, pos.coords.accuracy);
+    source.clear(true);
+    source.addFeatures([
+      new ol.Feature(accuracy.transform('EPSG:4326', map.getView().getProjection())),
+      new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat(coords)))
+    ]);
+  }, function (error) {
+    alert("matchPosition(): " + error.message);
+  }, {
+    enableHighAccuracy: true
+  });
+
+// https://github.com/jonataswalker/ol-geocoder
   const geocoder = new Geocoder('nominatim', {
     provider: 'osm',
     lang: 'en',
@@ -34,7 +58,7 @@ Map.init = function (lat, lng, zoom) {
 
   map.addControl(geocoder);
 
-  geocoder.on('addresschosen', function(evt){
+  geocoder.on('addresschosen', function (evt) {
     // const feature = evt.feature;
     // const coord = evt.coordinate;
     // const address = evt.address;
@@ -49,11 +73,13 @@ Map.init = function (lat, lng, zoom) {
   });
 
   return map;
-};
+}
+;
 
 // https://medium.com/attentive-ai/working-with-openlayers-4-part-1-creating-the-first-application-9ab27bbd7a62
 $(function () {
-  const map = Map.init(45.490972, -93.648045, 10);
+  // const map = Map.init(45.490972, -93.648045, 10);
+  const map = Map.init(0, 0, 10);
 
   const markerVectorLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
